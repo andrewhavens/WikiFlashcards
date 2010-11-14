27136326a837e5303f8d216ddd66038ecdbb8be3
Andrew Havens <misbehavens@gmail.com>
2010-11-14 13:33:05 -0800

    added jquery ui styles. some javascript nicities. and some other styles

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController  
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:index, :show]
   
   def index
     @books = Book.all
