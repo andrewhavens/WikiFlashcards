3f38c3f241bf008a1769a84e8ea4cf5a7b5b1c78
Andrew Havens <email@andrewhavens.com>
2011-08-09 22:09:50 -0700

    Moved history diff into application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def history_diff(card_changeset)
+		result = card_changeset.map do |k,v|
+			content_tag(:span, v[0], :class => "old_value") + ' to ' + content_tag(:span, v[1], :class => "new_value")
+		end
+		result.to_sentence.html_safe
+	end
 end
